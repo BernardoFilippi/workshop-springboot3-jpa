59e3ab3d9f9f4a8b5e434e811dd10fd85e11fe54
Bernardo Antônio Filippi <kirfe06@gmail.com>
2023-07-24 14:28:48 -0300

    h2 db

--- src/main/java/com/aulasts/course/entities/User.java
+++ src/main/java/com/aulasts/course/entities/User.java
@@ -2,10 +2,17 @@ package com.aulasts.course.entities;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
 	
 	private static final long serialVersionUID = 1L;
+	@Id
 	private Long id;
 	private String name;
 	private String email;
