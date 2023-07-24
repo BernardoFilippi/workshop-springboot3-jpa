0db9338c4a34209dc07bbdea37d1213a71dd7c4d
Bernardo Antônio Filippi <kirfe06@gmail.com>
2023-07-24 16:10:13 -0300

    Database seeding, dependency injection

---
+++ src/main/java/com/aulasts/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.aulasts.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.aulasts.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
